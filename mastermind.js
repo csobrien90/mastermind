183c2fe36a58b3db03b8637dd335fba6b829e0d7
Chad O'Brien <obrien.music@gmail.com>
2021-05-13 12:11:03 -0400

    Change title in main head and add associated js file.

---
+++ mastermind.js
@@ -0,0 +1,41 @@
+
+const input = document.querySelector('button');
+const instructions = document.getElementById('instructions');
+const challenge = document.querySelector('select');
+
+input.addEventListener('click', setCode);
+
+function setCode() {
+    let difficulty = challenge.value;
+    var code = generateCode(difficulty);
+    alert(code);
+}
+
+function generateCode(difficulty) {
+    let code;
+    let howTo;
+    if (difficulty == "easy") {
+        code = getRandomInt(3) + getRandomInt(3) + getRandomInt(3);
+        howTo = "Easy Mode: Guess the three digit code. Each digit is between 1 and 3.";
+    } else if (difficulty == "medium") {
+        code = getRandomInt(9) + getRandomInt(9) + getRandomInt(9) + getRandomInt(9);
+        howTo = "Medium Mode: Guess the four digit code. Each digit is between 1 and 9.";
+    } else {
+        code = randomLetter() + randomLetter() + randomLetter() + randomLetter();
+        howTo = "Hard Mode: Guess the sequence of four random lowercase letters.";
+    }
+    instructions.innerHTML = howTo;
+    return(code);
+}
+
+function getRandomInt(max) {
+    let int = 1 + Math.floor(Math.random() * max);
+    let stringInt = int.toString();
+    return(stringInt);
+  }
+
+function randomLetter() {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let randomizer = +getRandomInt(26) - 1;
+    return(alphabet[randomizer]);
+  }
